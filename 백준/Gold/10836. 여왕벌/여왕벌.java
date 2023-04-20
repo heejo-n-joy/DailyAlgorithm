1fb0e3b3733cfa17d9bdbdc57b31576436783bfe
heejo <joy96817@naver.com>
2023-04-20 14:39:52 +0900

    [Gold IV] Title: 여왕벌, Time: 1736 ms, Memory: 300308 KB -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.java
+++ 백준/Gold/10836. 여왕벌/여왕벌.java
@@ -1,16 +1,26 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
  *
  * 요구하는 출력 : 애벌레들이 성장한 후 마지막 저녁 애벌레들의 크기
  *
- * 풀이 방법 : 시뮬레이션
+ * 잘못된 풀이 방법 : 시뮬레이션(시간초과)
  * - 1. 애벌레가 들어있는 벌집과 똑같은 사이즈의 배열을 만든다.
  * - 2. 해당 배열에 각 애벌레가 얼마만큼 성장하는지 작성한다.
  * - 3. 구한 값만큼 벌집의 애벌레에 반영한다.
+ * => O(M*M*N)이라는 말도안되는 시간초과가 발생
+ *
+ * 보완
+ * - 벌집도 만들 필요 없다.
+ * - 맨 왼쪽 열을 제외하고, 나머지 모든 값은 맨 위의 행의 증가 에너지를 그대로 받기 때문이다.
+ * - 맨 왼쪽 열의 에너지를 저장하는 1차원 배열, 맨 위의 행의 에너지를 저장하는 1차원 배열을 만들어, 그 곳에만 값을 갱신한다.
+ * - 출력할 때만 값을 더해주면 O(N*M)이라는 시간 복잡도로 간단하게 구할 수 있다.
+ *
+ * 보완2
+ * - 더 줄여야 한다..
+ * - 에너지 0인 경우에는 과감하게 패스해버리자
  */
 
 public class Main {
@@ -23,52 +33,17 @@ public class Main {
         int M = Integer.parseInt(stringTokenizer.nextToken());    //벌집의 크기
         int N = Integer.parseInt(stringTokenizer.nextToken());    //날짜 수
 
-        int[][] original_map = new int[M][M];   //벌집
-        for(int i = 0; i<M; i++){
-            for(int j = 0; j<M; j++){
-                original_map[i][j] = 1;
-            }
-        }
-
+        int[] temp_map = new int[2*M -1];
         for(int day =0; day<N; day++){
-            int[][] temp_map = new int[M][M];   //계산용 맵
 
             //제일 왼쪽 열과 제일 왼쪽 행의 애벌레들의 성장 에너지를 반영한다.
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int[] increase_count = new int[3];  //주어진 0,1,2 에너지의 개수를 담는다.
-            for(int i = 0; i<3; i++){
-                increase_count[i] = Integer.parseInt(stringTokenizer.nextToken());
-            }
-            int increase_index = 0;
-            for(int i = 0; i < M * 2 -1; i++){
-                //혹시 현재 크기의 에너지 개수가 0개라면
-                while(increase_count[increase_index] == 0){
-                    //다음 크기의 에너지로 넘어간다.
-                    increase_index++;
-                }
-                //(0,0)으로 왔다면, 이제는 행으로(우측으로) 이동한다.
-                if(i >= M){
-                    temp_map[0][i-M + 1] = increase_index;
-                }
-                //(0,0)에 오지 않았다면, 열로(위로) 이동한다.
-                else {
-                    temp_map[M-1-i][0] = increase_index;
-                }
-                //에너지 개수를 1 감소한다.
-                increase_count[increase_index]--;
-            }
-
-            //나머지 애벌레들의 성장 에너지를 계산한다.
-            for(int i = 1; i<M; i++){
-                for(int j = 1; j<M; j++){
-                    temp_map[i][j] = calcMax(temp_map[i-1][j-1], temp_map[i][j-1], temp_map[i-1][j]);
-                }
-            }
-
-            //모든 애벌레들의 성장 에너지를 기존에서 더한다.
-            for(int i = 0; i<M; i++){
-                for(int j = 0; j<M; j++){
-                    original_map[i][j] += temp_map[i][j];
+            int index = Integer.parseInt(stringTokenizer.nextToken());   //값이 0만큼 패스한다.
+            for(int energy = 1; energy<=2; energy++){
+                int rest_energy = Integer.parseInt(stringTokenizer.nextToken());    //에너지 1과 2의 개수
+                while(rest_energy > 0){
+                    temp_map[index++] += energy;
+                    rest_energy--;
                 }
             }
         }
@@ -76,15 +51,18 @@ public class Main {
         //결과 출력
         for(int i =0; i<M; i++){
             for(int j = 0; j<M; j++){
-                sb.append(original_map[i][j] + " ");
+                //열
+                if(j == 0){
+                    sb.append((1 + temp_map[M-1 - i]) + " ");
+                }
+                //행
+                else{
+                    sb.append((1 + temp_map[M - 1 + j]) + " ");
+                }
             }
             sb.append("\n");
         }
 
         System.out.println(sb.toString());
-    }
-
-    public static int calcMax(int a, int b, int c){
-        return Math.max(a, Math.max(b, c));
     }
 }
